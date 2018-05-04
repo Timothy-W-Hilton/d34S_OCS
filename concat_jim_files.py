4c01782d15a2435d93cb95f3bfd26647369b1342
Timothy W. Hilton <thilton@ucmerced.edu>
2018-05-04 13:32:26 -0700

    create single monthly avg anthro [OCS] file from daily files of hourly [OCS]

--- concat_jim_files.py
+++ concat_jim_files.py
@@ -1,6 +1,9 @@
 import numpy as np
+import os
 import os.path
 from glob import glob
+from nco import Nco
+import calendar
 
 def avg_anthro(p, workdir):
     """average daily files of hourly [OCS] into monthly means
@@ -12,16 +15,46 @@ def avg_anthro(p, workdir):
     ARGS:
     p (str): full path to anthro root directory
     """
+    nco = Nco()
     annual_dirs = sorted(glob(os.path.join(p, "20[01]*")))
-    print('annual dirs: {}'.format(annual_dirs))
-    for this_year in annual_dirs:
+    all_monthly_files = []
+    for this_dir in annual_dirs:
+        this_year = os.path.basename(this_dir)
         for this_month in np.arange(1, 13):
-            this_wildcard = os.path.join(this_year,
+            this_wildcard = os.path.join(this_dir,
                                          "{:02d}".format(this_month),
                                          "*.nc")
+            ndays = calendar.monthrange(int(this_year), this_month)[1]
             files = sorted(glob(this_wildcard))
-            print(this_year, this_month, files)
-
+            # ignore incomplete months
+            if len(files) == ndays:
+                new_fname = os.path.join(workdir,
+                                         "{}-{:02d}_anthro_avg.nc".format(
+                                             this_year, this_month))
+                print('creating {}... '.format(new_fname), end='')
+                #average hourly data on time dimension
+                nco.nces(input=files, output=new_fname, options=['-d time,0'])
+                #add variables for year and month
+                nco.ncap2(input=new_fname,
+                          output=new_fname,
+                          options=['-s year[time]={}'.format(this_year)])
+                nco.ncap2(input=new_fname,
+                          output=new_fname,
+                          options=['-s month[time]={}'.format(this_month)])
+                # 'time' variable, for time of day, now contains 0 at
+                # all timestep and is meaningless, so remove it
+                nco.ncks(input=new_fname,
+                         output=new_fname,
+                         options=['-x -v time'])
+                print('done')
+                all_monthly_files.append(new_fname)
+    nco.ncecat(input=all_monthly_files,
+               output=os.path.join(out_dir, "anthro.nc"),
+               options=['-u tstep'])
+    print('created anthro.nc')
+    print('removing individual monthly files')
+    for this_file in all_monthly_files:
+        os.remove(this_file)
 
 if __name__ == "__main__":
     """
@@ -29,4 +62,6 @@ if __name__ == "__main__":
     anthro_dir = os.path.join('/', 'global', 'cscratch1', 'sd',
                               'jstineci', 'seasonal_amplitude',
                               'anthro_flux_only')
-    avg_anthro(anthro_dir, None)
+    out_dir = os.path.join('/', 'global', 'cscratch1', 'sd',
+                          'twhilton', 'global_sandbox')
+    avg_anthro(anthro_dir, out_dir)
