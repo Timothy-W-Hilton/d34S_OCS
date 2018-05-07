aec5ee21eba5a586f2a2fedbcf52debfda1ad004
Timothy W. Hilton <thilton@ucmerced.edu>
2018-05-07 14:22:06 -0700

    total anthro, ocean fluxes

--- calc_jim_flux_totals.py
+++ calc_jim_flux_totals.py
@@ -21,22 +21,27 @@ def get_secs_per_month(year):
                      for mon in range(1, 13)])
            * secs_per_day)
 
-def get_anthro_fluxes(fluxes_dim=(12, 91, 144)):
+def get_fluxes(dir, year, fluxes_dim=(12, 91, 144)):
     """read a year's worth of monthly OCS fluxes and calculate annual total
     """
-    fnames = [os.path.join(anthro_dir, "{:02d}.nc".format(mon))
+    fnames = [os.path.join(dir, "{:02d}.nc".format(mon))
                            for mon in range(1, 13)]
     fluxes = np.zeros(fluxes_dim)
     for this_mon in range(12):
         nc = netCDF4.Dataset(fnames[this_mon])
         fluxes[this_mon, ...] = nc.variables['COS_Flux'][...]
         nc.close()
-    secs_per_month = get_secs_per_month(2015)
+    secs_per_month = get_secs_per_month(year)
     total = (secs_per_month[:, np.newaxis, np.newaxis] * fluxes).sum(axis=0)
     return(total)
+
 
 if __name__ == "__main__":
     years = range(2004, 2008)
     df = pd.DataFrame({'anthro': None, 'ocean':None}, index=years)
     for this_year in years:
-        df.anthro[this_year] = get_anthro_fluxes().sum()
+        df.anthro[this_year] = get_fluxes(anthro_dir, this_year).sum()
+        this_ocean_dir = os.path.join(ocean_dir, "{:04d}".format(this_year))
+        df.ocean[this_year] = get_fluxes(this_ocean_dir, this_year).sum()
+
+    # fluxes are in kG S/Km^2/S -- convert kg S to gG S
