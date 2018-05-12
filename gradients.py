implement OCS anomaly calculation.  currently incorrect (see TODO)

gradients.py
@@ -1,7 +1,7 @@
 import numpy as np
 import netCDF4
 
-def parse_ocean(infile='./ocean.nc'):
+def get_ocean_anomaly(infile='./ocean.nc'):
     """read anthro [OCS] into 48x191x90 array
     """
     nc = netCDF4.Dataset(infile)
@@ -9,18 +9,20 @@ def parse_ocean(infile='./ocean.nc'):
     OCS = OCS.reshape( 48, 144, 91, 47)
     OCS = np.rollaxis(OCS, 3, 1)
     OCS = np.rollaxis(OCS, 3, 2)
+    OCS = OCS - OCS.mean() # TODO: need apply_over_axis or apply_along_axis
     nc.close()
     return(OCS)
 
-def parse_anthro(infile='./anthro.nc'):
+def get_anthro_anomaly(infile='./anthro.nc'):
     """read anthro [OCS] into 48x191x90 array
     """
     nc = netCDF4.Dataset(infile)
     OCS = nc.variables['COS'][...].squeeze()
+    OCS = OCS - OCS.mean()
     nc.close()
     return(OCS)
 
 if __name__ == "__main__":
 
-    ocs_anthro = parse_anthro()
-    ocs_ocean = parse_ocean()
+    ocs_anthro = get_anthro_anomaly()
+    ocs_ocean = get_ocean_anomaly()