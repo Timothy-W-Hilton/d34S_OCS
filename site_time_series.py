3d664ea2e6fc942d863e5dde6494ba86ba041751
Timothy W. Hilton <thilton@ucmerced.edu>
2018-06-01 12:53:50 -0600

    show d34S on plots rather than [OCS]

--- site_time_series.py
+++ site_time_series.py
@@ -13,6 +13,13 @@ def get_site_xy(code):
            sites[sites.Code == code].X.values)
 
 
+def calc_34S_concentration(ocs, permil_34S):
+    """calculate concentration of 34S for a specified fractionation
+
+    ocs (array-like): OCS concentration (ppt)
+    permil_34S (float): 34S fractionation
+    """
+    return(ocs * (permil_34S / 1000.0))
 
 if __name__ == "__main__":
     ocs_anthro = gradients.get_anthro_anomaly()
@@ -27,17 +34,19 @@ if __name__ == "__main__":
 
     for k, this_data in {'ocean': ocs_ocean, 'anthro':ocs_anthro}.items():
         plt.figure()
-        for this_site in indian_gradient:
+        for this_site in ('MLO', ):
             this_x, this_y = get_site_xy(this_site)
             # show S hemisphere sites with dashed lines, N hemisphere with solid
             if (all_sites[all_sites.Code == this_site].Latitude < 0.0).all():
                 linestyle='dashed'
             else:
                 linestyle='solid'
-            plt.plot(this_data.data[:, 0, this_x, this_y] * TO_PPT,
+            plt.plot(calc_34S_concentration(
+                ocs=this_data.data[:, 0, this_x, this_y],
+                permil_34S=9.0) * TO_PPT,
                      linestyle=linestyle,
                      label=this_site)
-        plt.gca().set_title('{} OCS anomaly (from global mean)'.format(k))
+        plt.gca().set_title(r'{} $\delta$34S OCS anomaly (from global mean)'.format(k))
         plt.gca().set_xlabel('month')
-        plt.gca().set_ylabel('[OCS] (ppt)')
+        plt.gca().set_ylabel(r'$\delta$34S [OCS] (ppt)')
         plt.legend()
