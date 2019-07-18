c0dd93af574b1eda27b52a94d26c7d20d5fbefbf
Timothy W. Hilton <twhilton@ucsc.edu>
2019-07-18 11:58:47 -0600

    handle empty-slice warning in nanmean
    - Some global slices of ocean, anthro fluxes are all NaN.
      np.nanmean (reasonably) returns NaN for these, so no need for the
      warning message to clutter the output.

--- gradients.py
+++ gradients.py
@@ -1,5 +1,7 @@
 import numpy as np
 import netCDF4
+import warnings
+
 
 def get_ocean_anomaly(infile='./ocean.nc'):
     """read anthro [OCS] into 48x191x90 array
@@ -10,7 +12,11 @@ def get_ocean_anomaly(infile='./ocean.nc'):
     OCS = np.rollaxis(OCS, 3, 1)
     OCS = np.rollaxis(OCS, 3, 2)
     # subtract time-varying global spatial mean OCS to get anomalies
-    OCS = OCS - np.apply_over_axes(np.nanmean, OCS, (1, 2, 3))
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore",
+                                category=RuntimeWarning,
+                                message="Mean of empty slice")
+        OCS = OCS - np.apply_over_axes(np.nanmean, OCS, (1, 2, 3))
     nc.close()
     return(OCS)
 
@@ -20,7 +26,11 @@ def get_anthro_anomaly(infile='./anthro.nc'):
     nc = netCDF4.Dataset(infile)
     OCS = nc.variables['COS'][...].squeeze()
     # subtract time-varying global spatial mean OCS to get anomalies
-    OCS = OCS - np.apply_over_axes(np.nanmean, OCS, (1, 2, 3))
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore",
+                                category=RuntimeWarning,
+                                message="Mean of empty slice")
+        OCS = OCS - np.apply_over_axes(np.nanmean, OCS, (1, 2, 3))
     nc.close()
     return(OCS)
 
