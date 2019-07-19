1f4d6092e18c0b30ca2968ee64b45ac97f5c5cf1
Timothy W. Hilton <twhilton@ucsc.edu>
2019-07-19 17:14:43 -0600

    temporarily comment out site gradient calculations; minor syntax fixes

--- site_time_series.py
+++ site_time_series.py
@@ -20,6 +20,7 @@ def epsilon_to_uptake_heavy(epsilon, R, U_light):
     isotope
     """
     U_heavy = U_light * (1.0 + (epsilon_U / 1000.0)) * R
+    return(U_heavy)
 
 
 """ ocean adjustment factor
@@ -61,11 +62,12 @@ fractionation = {'ocean': 19,
                  'oxidation': 8}
 # d34S fractionation uncertainty ranges assumed in text of BSF
 # proposal (pp 7-8)
-fractionation_uncert  = {'ocean': 0,
-                         'anthro': 2,
-                         'plant': 0,
-                         'soil': 1,
-                         'oxidation': 0}
+fractionation_uncert = {'ocean': 0,
+                        'anthro': 2,
+                        'plant': 0,
+                        'soil': 1,
+                        'oxidation': 0}
+
 
 def get_site_xy(code):
     """return site info for one specified site
@@ -84,44 +86,53 @@ def calc_34S_concentration(ocs, permil_34S):
     """
     return(ocs * (permil_34S / 1000.0))
 
+
 if __name__ == "__main__":
     ocs_anthro = gradients.get_anthro_anomaly()
     ocs_ocean = gradients.get_ocean_anomaly()
 
     all_sites = gather_sites_data()
     pacific_gradient = list(all_sites[(all_sites.Longitude > 150.0) |
-                              (all_sites.Longitude < -150.0)].Code)
+                                      (all_sites.Longitude < -150.0)].Code)
     atlantic_gradient = ['ZEP', 'ICE', 'MHD', 'AZR', 'IZO',
-                    'ASC', 'HBA', 'NMB', 'CPT']
+                         'ASC', 'HBA', 'NMB', 'CPT']
     indian_gradient = ['SEY', 'CRZ', 'SYO']
 
-    plt.figure()
-    for this_site in pacific_gradient:  # ('CRZ', ):
-        this_x, this_y = get_site_xy(this_site)
-        # show S hemisphere sites with dashed lines, N hemisphere with solid
-        if (all_sites[all_sites.Code == this_site].Latitude < 0.0).all():
-            linestyle='dashed'
-        else:
-            linestyle='solid'
-        #TODO: does calc_d34S()  need correction for isotope arithmetic?
-        ocean_d34S = calc_34S_concentration(
-            ocs=ocs_ocean.data[:, SFC_Z, this_x, this_y],
-            permil_34S=fractionation['ocean']) * TO_PPT
-        anthro_d34S = calc_34S_concentration(
-            ocs=ocs_anthro.data[:, SFC_Z, this_x, this_y],
-            permil_34S=fractionation['anthro']) * TO_PPT
-        #TODO: these next three lines need correction for isotope
-        #arithmetic I think
-        hi_ocean_flux_d34S = ocean_d34S * ocean_flux['Launois_high']
-        med_ocean_flux_d34S = ocean_d34S * ocean_flux['Launois_best']
-        low_ocean_flux_d34S = ocean_d34S * ocean_flux['Lennartz']
-        plt.errorbar(np.arange(med_ocean_flux_d34S.size),
-                     (med_ocean_flux_d34S / anthro_d34S).squeeze(),
-                     yerr=np.array([low_ocean_flux_d34S / anthro_d34S,
-                                    hi_ocean_flux_d34S / anthro_d34S]).squeeze(),
-                     linestyle=linestyle,
-                     label=this_site)
-    plt.gca().set_title(r'ratio ocean:anthro $\delta$34S OCS anomaly (from global mean)')
-    plt.gca().set_xlabel('month')
-    plt.gca().set_ylabel(r'$\delta$34S [OCS] (ppt)')
-    plt.legend()
+    model = ForwardS34Model(ocs_ocean, ocs_anthro)
+
+
+
+
+
+
+
+    # plt.figure()
+    # for this_site in pacific_gradient:  # ('CRZ', ):
+    #     this_x, this_y = get_site_xy(this_site)
+    #     # show S hemisphere sites with dashed lines, N hemisphere with solid
+    #     if (all_sites[all_sites.Code == this_site].Latitude < 0.0).all():
+    #         linestyle='dashed'
+    #     else:
+    #         linestyle='solid'
+    #     #TODO: does calc_d34S()  need correction for isotope arithmetic?
+    #     ocean_d34S = calc_34S_concentration(
+    #         ocs=ocs_ocean.data[:, SFC_Z, this_x, this_y],
+    #         permil_34S=fractionation['ocean']) * TO_PPT
+    #     anthro_d34S = calc_34S_concentration(
+    #         ocs=ocs_anthro.data[:, SFC_Z, this_x, this_y],
+    #         permil_34S=fractionation['anthro']) * TO_PPT
+    #     #TODO: these next three lines need correction for isotope
+    #     #arithmetic I think
+    #     hi_ocean_flux_d34S = ocean_d34S * ocean_flux['Launois_high']
+    #     med_ocean_flux_d34S = ocean_d34S * ocean_flux['Launois_best']
+    #     low_ocean_flux_d34S = ocean_d34S * ocean_flux['Lennartz']
+    #     plt.errorbar(np.arange(med_ocean_flux_d34S.size),
+    #                  (med_ocean_flux_d34S / anthro_d34S).squeeze(),
+    #                  yerr=np.array([low_ocean_flux_d34S / anthro_d34S,
+    #                                 hi_ocean_flux_d34S / anthro_d34S]).squeeze(),
+    #                  linestyle=linestyle,
+    #                  label=this_site)
+    # plt.gca().set_title(r'ratio ocean:anthro $\delta$34S OCS anomaly (from global mean)')
+    # plt.gca().set_xlabel('month')
+    # plt.gca().set_ylabel(r'$\delta$34S [OCS] (ppt)')
+    # plt.legend()
