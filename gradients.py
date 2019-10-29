906c76262cbdd4eb26068000dc5cffac74aea1d0
Timothy W. Hilton <twhilton@ucsc.edu>
2019-10-29 10:18:40 -0600

    change netCDF backend from netCDF4 to xarray

--- gradients.py
+++ gradients.py
@@ -1,26 +1,37 @@
 import numpy as np
 import netCDF4
 import warnings
+import xarray as xr
+
 
 def get_ocean_production(infile='./ocean.nc'):
     """read ocean [OCS] into 48x191x90 array
     """
-    nc = netCDF4.Dataset(infile)
+    ocean = xr.open_dataset(infile)
+    ocean = ocean.assign_coords(time=range(ocean.year.size * ocean.month.size),
+                                xid=ocean.xid,
+                                yid=ocean.yid,
+                                zid=ocean.zid,
+                                year=ocean.year)
+    ocean['OCS_y_m'] = ocean['OCS']
+    ocean['OCS'] = xr.Variable(('time', 'zid', 'yid', 'xid'),
+                               np.full((ocean.time.size,
+                                        ocean.zid.size,
+                                        ocean.yid.size,
+                                        ocean.xid.size),
+                                       fill_value=np.nan))
     # read OCS from file.  It has 4 years of data with years, months
     # as separate axes
-    OCS_y_m = nc.variables['OCS'][...].squeeze()
     # copy the data into a new array with a continuous time axis
     # OCS_y_m = np.rollaxis(OCS_y_m, 3, 1)
     # OCS_y_m = np.rollaxis(OCS_y_m, 3, 2)
-    OCS = np.full((48, 47, 91, 144), fill_value=np.nan)
     idx = 0
-    for y in range(OCS_y_m.shape[0]):
-        for m in range(OCS_y_m.shape[1]):
-            OCS[idx, ...] = OCS_y_m[y, m, ...]
+    for y in range(ocean.OCS_y_m.year.size):
+        for m in range(ocean.OCS_y_m.month.size):
+            ocean.OCS[idx, ...] = ocean.OCS_y_m[y, m, ...]
             idx = idx + 1
-    nc.close()
-    OCS = np.ma.masked_invalid(OCS)
-    return(OCS)
+    return(ocean)
+
 
 def get_ocean_anomaly(infile='./ocean.nc', OCS=None):
     """read ocean [OCS] into 48x191x90 array
@@ -35,8 +46,9 @@ def get_ocean_anomaly(infile='./ocean.nc', OCS=None):
         warnings.filterwarnings("ignore",
                                 category=RuntimeWarning,
                                 message="Mean of empty slice")
-        OCS_anom = OCS - np.apply_over_axes(np.nanmean, OCS, (1, 2, 3))
-    return(OCS_anom)
+        OCS_anom = OCS['OCS'].mean(skipna=True, dim=('xid', 'yid', 'zid'))
+    return(OCS_anom.values)
+
 
 def get_anthro_production(infile='./anthro.nc'):
     """read anthro [OCS] into 48x191x90 array
@@ -45,6 +57,7 @@ def get_anthro_production(infile='./anthro.nc'):
     OCS = nc.variables['COS'][...].squeeze()
     nc.close()
     return(OCS)
+
 
 def get_anthro_anomaly(infile='./anthro.nc'):
     """read anthro [OCS] into 48x191x90 array
@@ -61,6 +74,7 @@ def get_anthro_anomaly(infile='./anthro.nc'):
                                 message="Mean of empty slice")
         OCS_anom = OCS - np.apply_over_axes(np.nanmean, OCS, (1, 2, 3))
     return(OCS_anom)
+
 
 if __name__ == "__main__":
     ocs_anthro = get_anthro_production()
