67b55423b63c7383be72af197e1f0b0fbd8b95f0
Timothy W. Hilton <twhilton@ucsc.edu>
2019-12-05 14:16:32 -0700

    move GEOS-Chem OCS coord/data rearrangement to format_dataset

--- toy_problem.py
+++ toy_problem.py
@@ -51,15 +51,29 @@ def regrid_fcos(fcos, new_lon, new_lat):
     return(fcos_regridded)
 
 
+def format_dataset(this_dataset):
+    """promote longitude, latitude, lev from variable to coordinate
+    variable; create a new coordinate variable tstep
+    """
+    this_dataset = this_dataset.assign_coords(
+        longitude=this_dataset.longitude[0],
+        latitude=this_dataset.latitude[0],
+        lev=this_dataset.lev[0],
+        tstep=xr.Variable(dims='tid',
+                          data=range(this_dataset.dims['tid'])))
+    return(this_dataset)
+
+
 def main():
-
-
     global_conc_dir = os.path.join('/', 'Users', 'tim', 'work',
                                    'Data', 'Jim_global')
     ocean_conc, plant_conc = (xr.open_mfdataset(
         os.path.join(global_conc_dir, this_dir, 'out_monthly', '*.nc'),
         concat_dim='tid',
         combine='nested') for this_dir in ['ocean_only', 'plant_only'])
+    ocean_conc = format_dataset(ocean_conc)
+    plant_conc = format_dataset(plant_conc)
+
     anthro_conc = xr.open_dataset(os.path.join(global_conc_dir, 'anthro.nc'))
     # remove singleton dimemsion time.  the timestamp is still present
     # in tstep
@@ -67,19 +81,21 @@ def main():
     # make tstep a coordinate
     anthro_conc = anthro_conc.assign_coords(
         tstep=range(anthro_conc.dims['tstep']))
+    anthro_conc.rename({'lat': 'latitude',
+                        'lon': 'longitude'})
 
     ocean_flux_kettle_grid = parse_kettle_flux(
         os.path.join('/', 'Users', 'tim', 'work', 'Data',
                      'kettle_fluxes', 'ocean_cos.dat'))
-    grid_lon, grid_lat = np.meshgrid(ocean_conc['longitude'].values[0, ...],
-                                     ocean_conc['latitude'].values[0, ...])
+    grid_lon, grid_lat = np.meshgrid(ocean_conc['longitude'].values,
+                                     ocean_conc['latitude'].values)
     ocean_flux = regrid_fcos(ocean_flux_kettle_grid, grid_lon, grid_lat)
 
     da_of = xr.DataArray(ocean_flux,
                          coords={'month': range(12),
-                                 'lon': ocean_conc.longitude.values[0, :],
-                                 'lat': ocean_conc.latitude.values[0, :]},
-                         dims=['month', 'lat', 'lon'],
+                                 'longitude': ocean_conc.longitude.values,
+                                 'latitude': ocean_conc.latitude.values},
+                         dims=['month', 'latitude', 'longitude'],
                          name='ocean_flux')
 
     return(da_of, ocean_flux, ocean_flux_kettle_grid,
