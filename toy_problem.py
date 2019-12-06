9dd52b7299445934f6859dac0f9401d11c9b6b7b
Timothy W. Hilton <twhilton@ucsc.edu>
2019-12-06 14:34:25 -0700

    downsample anthro fluxes from 1 x 1 grid to 2.5 x 2 grid
    - also convert pmol m-2 s-1 to pmol gridcell-1 month-1

--- toy_problem.py
+++ toy_problem.py
@@ -5,8 +5,23 @@ import re
 import os
 import fortranformat
 import idwnn_kdtree_interp as interp
+import geoviews as gv
+import xesmf as xe
+from geoviews.operation.regrid import weighted_regrid
+import calendar
 
+from stem_pytools.domain import calc_grid_area
 from holoviews.operation.datashader import regrid
+
+DEBUG_FLAG = False
+SECS_PER_MINUTE = 60
+MINS_PER_HOUR = 60
+HOURS_PER_DAY = 24
+MONTHS_PER_YEAR = 12
+SECS_PER_DAY = SECS_PER_MINUTE * MINS_PER_HOUR * HOURS_PER_DAY
+DAYS_PER_MONTH = np.array([calendar.monthrange(2012, m)[1]
+                           for m in range(1, 13)])
+SECS_PER_MONTH = SECS_PER_DAY * DAYS_PER_MONTH
 
 def parse_kettle_flux(fname):
     f = open(fname, 'r')
@@ -92,20 +107,73 @@ def get_andrew_antho_cos(new_lon1d, new_lat1d):
         {'Total_Anth._COS': 'anthro_flux',
          'lon': 'longitude',
          'lat': 'latitude'})
-    anth_ocs_high_res = anth_ocs_high_res.assign_coords(
+    anth_ocs_raw = anth_ocs_high_res.assign_coords(
         longitude=anth_ocs_high_res.longitude,
         latitude=anth_ocs_high_res.latitude,
         years=anth_ocs_high_res.latitude)
+    # This (below) dataset format works for plotting for reasons I do
+    # not yet understand
+    anth_highres = xr.Dataset(
+        data_vars= {'anthro_flux': (('year', 'latitude', 'longitude'),
+                                    anth_ocs_raw['anthro_flux'])},
+        coords= {'longitude': (('longitude'),
+                               anth_ocs_raw['longitude']),
+                 'latitude': (('latitude'),
+                              anth_ocs_raw['latitude']),
+                 'year': (('year'),
+                          range(anth_ocs_raw['t'].size)),
+                 'month': (('month'),
+                           range(1, 13))})
+    #regrid adapted from
+    #http://geoviews.org/user_guide/Resampling_Grids.html
+    gvds = gv.Dataset(anth_highres.sel(year=32),
+                      kdims=['longitude', 'latitude'])
+    grid = xe.util.grid_2d(new_lon1d.min() - 1.25,
+                           new_lon1d.max() + 1.25,
+                           2.5,
+                           new_lat1d.min() - 1.0,
+                           new_lat1d.max() + 1.0,
+                           2.0)
+    target = gv.Dataset(grid, kdims=['lon', 'lat'])
+    anth_lowres = weighted_regrid(gvds, target=target, streams=[]).data
+    anth_lowres = anth_lowres.assign_coords(month=range(1, 13))
+    anth_lowres['anthro_flux'] = anth_lowres['anthro_flux'].assign_attrs(
+        units='pmol m-2 s-1')
 
-    # old_lon, old_lat = np.meshgrid(anth_ocs_high_res['lon'].values,
-    #                                anth_ocs_high_res['lat'].values)
-    # new_lon, new_lat = np.meshgrid(new_lon1d, new_lat1d)
-    # anth_geoschem_grid = interp.spherical_idw_kdtree_interp(
-    #     old_lon, old_lat,
-    #     new_lon, new_lat,
-    #     data=anth_ocs_high_res['Total_Anth._COS'].data[-1, ...],
-    #     n_nbr=3)
-    return(anth_ocs_high_res) # , anth_geoschem_grid)  #
+    #flux (pmol / m2/ s)
+    focs_permonth = xr.DataArray(
+        data=np.tile(anth_lowres['anthro_flux'].values / MONTHS_PER_YEAR,
+                     reps = (MONTHS_PER_YEAR, 1, 1)),
+        dims=(('month', 'y', 'x')))
+    #flux (pmol / m2/ month)
+    focs_permonth = focs_permonth * SECS_PER_MONTH[:, np.newaxis, np.newaxis]
+    #flux (pmol / gridcell / month)
+    area_per_gridcell = get_area_all_gridcells(target.data['lon'].data,
+                                               target.data['lat'].data)
+    focs_permonth = focs_permonth * area_per_gridcell[np.newaxis, ...]
+    focs_permonth = focs_permonth.assign_attrs(units='pmol / gridcell / month')
+    anth_lowres = anth_lowres.assign(anthro_flux_permonth=focs_permonth)
+
+    return(anth_highres, anth_lowres)
+
+def get_area_all_gridcells(lon, lat, d_lon = 2.5, d_lat = 2.0):
+    """calculate area in km^2 of every cell in two 2d arrays of lats, lons
+
+    assumes lat, lon specify the center of the cell
+    """
+    area = np.zeros(lat.shape)
+    for i in range(lat.shape[0]):
+        for j in range(lat.shape[1]):
+            area[i, j]  = calc_grid_area(lon[i, j] + (d_lon / 2.0),
+                                         lon[i, j] - (d_lon / 2.0),
+                                         lat[i, j] + (d_lat / 2.0),
+                                         lat[i, j] - (d_lat / 2.0))
+    area = area * 1e-6  # convert m^2 to km^2
+    if DEBUG_FLAG:
+        print("*LAUGH TEST* total Earth "
+              "area (km^2): {:0.2e}".format(area.sum()))
+    return(area)
+
 
 def main():
     global_conc_dir = os.path.join('/', 'Users', 'tim', 'work',
@@ -138,6 +206,6 @@ if __name__ == "__main__":
 
     (da_of, anthro_conc, ocean_conc, plant_conc) = main()
 
-    anth_highres = get_andrew_antho_cos(
+    anth_highres, anth_lowres = get_andrew_antho_cos(
         ocean_conc['longitude'].values,
         ocean_conc['latitude'].values)
